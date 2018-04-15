8b9dcf0a252420ada5b8e7d3dde4908a7da072b9
Italo <italojmf@gmail.com>
2018-04-15 19:57:09 -0300

    Finish 3rd view implementation

--- android/app/src/main/java/com/collegeproject1/MainApplication.java
+++ android/app/src/main/java/com/collegeproject1/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.sensors.RNSensorsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNSensorsPackage()
       );
     }
 
--- src/views/Exercicio3.js
+++ src/views/Exercicio3.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Accelerometer } from 'react-native-sensors';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,16 +18,58 @@ const styles = StyleSheet.create({
 });
 
 class Exercicio3 extends Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      acceleration: {
+        x: 'unknown',
+        y: 'unknown',
+        z: 'unknown',
+      },
+      showWarning: false,
+    };
+    this.accelerationObservable = new Accelerometer({
+      updateInterval: 100, // defaults to 100ms
+    });
   }
 
+  componentWillMount() {
+    const { showWarning } = this.state;
+
+    if (showWarning) {
+      return;
+    }
+
+    this.accelerationObservable.subscribe(newAcceleration => {
+      if (newAcceleration.x > 6 ||
+          newAcceleration.y < 6 ||
+          newAcceleration.z > 6) {
+        this.setState({ showWarning: true });
+      }
+      this.setState({
+        acceleration: newAcceleration,
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.accelerationObservable.stop();
+  }
   render() {
+    const { acceleration } = this.state;
+    const { showWarning } = this.state;
+
     return (
-      <View style={styles.container}>
-        <Text>View 3</Text>
-      </View>
+      showWarning ?
+        <Text>
+          STOP!
+        </Text>
+        :
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Acceleration:</Text>
+          <Text style={styles.instructions}>
+            { `X : ${Number(acceleration.x).toFixed(4)} / Y : ${Number(acceleration.y).toFixed(4)} / Z : ${Number(acceleration.z).toFixed(4)}` }
+          </Text>
+        </View>
     );
   }
 }
